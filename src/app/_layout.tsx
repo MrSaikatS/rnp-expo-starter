1ef0f68da309721fa8d24aad1647252a8af09e7b
Saikat Sardar <saikatsardar5@gmail.com>
2025-04-28 21:55:42 +0530

    TanStack Query setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,10 @@
 import { ThemeProvider } from "@react-navigation/native";
+import {
+  onlineManager,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import * as Network from "expo-network";
 import { SplashScreen, Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import { PaperProvider } from "react-native-paper";
@@ -56,9 +62,30 @@ const RootLayout = () => {
   // Hide the splash screen until loading is complete.
   SplashScreen.hideAsync();
 
+  // Online status management
+  onlineManager.setEventListener((setOnline) => {
+    const eventSubscription = Network.addNetworkStateListener((state) => {
+      setOnline(!!state.isConnected);
+    });
+    return eventSubscription.remove;
+  });
+
+  // Create a QueryClient Instance
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 5,
+      },
+    },
+  });
+
   return (
     <>
-      <RootLayoutNavigator />
+      <QueryClientProvider client={queryClient}>
+        <RootLayoutNavigator />
+      </QueryClientProvider>
     </>
   );
 };
