5b115f12192687829afb6b83d20199b56dbdf67a
Saikat Sardar <saikatsardar5@gmail.com>
2025-05-06 18:30:58 +0530

    removed jotai for incompatibility

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,7 +1,23 @@
+import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 const TabLayout = () => {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather
+              name="home"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default TabLayout;
